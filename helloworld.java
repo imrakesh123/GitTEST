cc4a07a6afa91570094362112823022c5a220786
Rakesh <infolectualrk@gmail.com>
2021-09-07 22:49:56 +0530

    ange

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java"); 
-	System.out.println("Hello Change"); 
+	System.out.println("Hello Change");
+System.out.println("Hello Java"); 
     }  
 }  
